364dc7dab68e8616899ff12f19ca68016b46f643
Prem <premprakashisonline@gmail.com>
2026-01-25 22:14:54 +0530

    Weird Python Episode 3

--- Weird Python/Episode 3.py
+++ Weird Python/Episode 3.py
@@ -1,9 +1,17 @@
-text = "I love Python"
-# print(text.find("Java"))
-# print(bool(-1))
-# print(bool(0))
+# YouTube Link: https://youtu.be/gvmrQUdUQjQ
 
+
+text = "I love Python"
+
+# Output is Jave Found
 if text.find("Java"):
     print("Java Found")
 else:
     print("Java not found")
+
+
+""" Explanation
+find method returns -1 if given string is not found. In Python, 0 is False while non-zero is True. 
+If you try bool(text.find("C++")), it is equivalent to bool(-1) which is True. 
+Thus, the if statement is Truthy and it prints Java Found
+"""
